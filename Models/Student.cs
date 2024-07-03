3a5671415cb00acf6758939af432946dd68fa659
phamquanghien <pqh.cntt@gmail.com>
2024-07-03 21:53:40 +0700

    Add_Package

--- Models/Student.cs
+++ Models/Student.cs
@@ -4,9 +4,7 @@ namespace QuanLyThiHUMG.Models
     {
         public required string StudentCode { get; set; }
         public required string LastName { get; set; }
-
         public required string FirstName { get; set; }
-
         public required string BirthDay { get; set; }
         public bool IsActive { get; set; } = true;
         public bool IsDelete { get; set; } = false;
